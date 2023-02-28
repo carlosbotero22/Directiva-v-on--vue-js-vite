797ffa9dc3fcc392f66b5550bca4fe635cd9eb16
carlosbotero22 <102599195+carlosbotero22@users.noreply.github.com>
2023-02-28 12:43:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Directiva-v-on--VUE-JS-VITE
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-jrexl5)
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{ title }}</h1>
+  <button v-on:click="activarBoton" class="btn btn-primary m-2">
+    Activame 1
+  </button>
+  <button @click="activarBoton" class="btn btn-danger m-2">Activame 2</button>
+</template>
+
+<script setup>
+const title = 'Vite + Vue';
+//metodo
+const activarBoton = () => {
+  console.log('me activaste');
+};
+</script>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
